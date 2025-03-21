7bd208c599835caa2cb7909f08c7f4c1f7996cce
lalolozadev <eloza.17912@gmail.com>
2025-03-20 18:02:59 -0600

    refactor: welcome window and config dir created

---
+++ config/button.py
@@ -0,0 +1,35 @@
+ini_size = (250, 50)
+
+padding = 15
+border_radius = 5
+
+start = ("QPushButton {"
+    "background-color: #4CAF50;"
+    "color: white;"
+    "font-size: 16px;"
+    f"border-radius: {border_radius}px;"
+    f"padding: {padding}px;"
+    "}"
+    "QPushButton:hover {"
+    "background-color: #45a049;"
+    "}"
+    "QPushButton:pressed {"
+    "background-color: #388e3c;"
+    "}"
+)
+
+doc = ("QPushButton {"
+    "background-color: #24292e;"
+    "color: white;"
+    "font-size: 16px;"
+    f"border-radius: {border_radius}px;"
+    f"padding: {padding}px;"
+    "}"
+    "QPushButton:hover {"
+    "background-color: #2b3137;"
+    "}"
+    "QPushButton:pressed {"
+    "background-color: #2b3137;"
+    "}"
+
+)
