a227e974c668ba4cf9993c0865132f5438cb83c4
lalolozadev <eloza.17912@gmail.com>
2025-03-25 12:01:43 -0600

    refactor: navigation buttons

--- windows/sidebar.py
+++ windows/sidebar.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, 
     QFileDialog, QLineEdit, QFrame, QStackedWidget, QComboBox, QLabel
 )
+import config.button as button
 
 # Clases de cada sección
 class LoadFileSection(QWidget):
@@ -11,6 +12,8 @@ class LoadFileSection(QWidget):
         
         self.btn_load = QPushButton("Load File")
         self.btn_load.clicked.connect(parent.select_file)
+        self.btn_load.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_load.setStyleSheet(button.next)
         self.file_path = QLineEdit()
 
         layout.addWidget(self.btn_load)
@@ -18,6 +21,8 @@ class LoadFileSection(QWidget):
         
         self.btn_next = QPushButton("Next")
         self.btn_next.clicked.connect(parent.next_section)
+        self.btn_next.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_next.setStyleSheet(button.next)
         layout.addWidget(self.btn_next)
         
         self.setLayout(layout)
@@ -28,14 +33,18 @@ class VariablesSection(QWidget):
         layout = QVBoxLayout(self)
         layout.addWidget(QLabel("Variables Section"))
         
-        self.btn_back = QPushButton("Back")
-        self.btn_back.clicked.connect(parent.previous_section)
-        
         self.btn_next = QPushButton("Next")
         self.btn_next.clicked.connect(parent.next_section)
+        self.btn_next.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_next.setStyleSheet(button.next)
 
-        layout.addWidget(self.btn_back)
+        self.btn_back = QPushButton("Back")
+        self.btn_back.clicked.connect(parent.previous_section)
+        self.btn_back.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_back.setStyleSheet(button.back)
+
         layout.addWidget(self.btn_next)
+        layout.addWidget(self.btn_back)
         self.setLayout(layout)
 
 class PlotSection(QWidget):
@@ -46,12 +55,16 @@ class PlotSection(QWidget):
         
         self.btn_back = QPushButton("Back")
         self.btn_back.clicked.connect(parent.previous_section)
+        self.btn_back.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_back.setStyleSheet(button.back)
         
         self.btn_next = QPushButton("Next")
         self.btn_next.clicked.connect(parent.next_section)
+        self.btn_next.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_next.setStyleSheet(button.next)
 
-        layout.addWidget(self.btn_back)
         layout.addWidget(self.btn_next)
+        layout.addWidget(self.btn_back)
         self.setLayout(layout)
 
 class ExportSection(QWidget):
@@ -62,6 +75,8 @@ class ExportSection(QWidget):
         
         self.btn_back = QPushButton("Back")
         self.btn_back.clicked.connect(parent.previous_section)
+        self.btn_back.setFixedSize(button.nav_size[0], button.nav_size[1])
+        self.btn_back.setStyleSheet(button.back)
         
         layout.addWidget(self.btn_back)
         self.setLayout(layout)
